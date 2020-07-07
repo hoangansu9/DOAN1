80b1b46f97ca3c47853f9b67ce998655f92b3bb7
hoangansu9 <55569782+hoangansu9@users.noreply.github.com>
2020-07-07 14:29:22 +0700

    fix

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace doan_1
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                     
                       "~/Content/site.css"));
         }
     }
